2be4d6652ac2e7d9a30dd53054665caee575201d
Sohil Athare <61813964+Sohil1926@users.noreply.github.com>
2023-11-10 22:20:20 -0500

    fixed path name

--- mainScraper.py
+++ mainScraper.py
@@ -1,2 +1,2 @@
 print("hello from")
-PATH = "/Users/sohilathare/Downloads/chrome-mac-arm64/ABOUT /Users/sohilathare/Downloads/chrome-mac-arm64/Google Chrome for Testing.app"
+PATH = "/Users/sohilathare/chromedriver"

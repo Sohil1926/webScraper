a4a9c4ff30d67d9166923dc08a6cfe8d1ac6070c
Sohil Athare <61813964+Sohil1926@users.noreply.github.com>
2023-11-10 22:00:10 -0500

    added link

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # webScraper
+Download latest version of Chrome Driver
+https://googlechromelabs.github.io/chrome-for-testing/#stable
+
---
+++ mainScraper.py
@@ -0,0 +1,2 @@
+print("hello from")
+PATH = "/Users/sohilathare/Downloads/chrome-mac-arm64/ABOUT /Users/sohilathare/Downloads/chrome-mac-arm64/Google Chrome for Testing.app"

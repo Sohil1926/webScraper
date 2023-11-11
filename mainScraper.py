21ffdf08be8796e02def4ea8986d70c22d5486a3
Sohil Athare <61813964+Sohil1926@users.noreply.github.com>
2023-11-11 10:09:29 -0500

    airtable api works

--- mainScraper.py
+++ mainScraper.py
@@ -1,30 +1,48 @@
+import requests
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+# Airtable setup
+personal_access_token = 'patVnh56FVKpYUssq.46c740675ec2caf3ada99151efac8cb7870fa823fd67b2ee1d5bf3ce0fe257b4'
+base_id = 'app11Wk2b9AkAnoLx'
+table_name = 'Table 1'
+airtable_url = f"https://api.airtable.com/v0/app11Wk2b9AkAnoLx/Table%201"
+
+headers = {
+    "Authorization": f"Bearer {personal_access_token}",
+    "Content-Type": "application/json"
+}
+
+# Selenium setup
 driver = webdriver.Chrome()
 driver.get("https://www.ggcatering.com/venues#?venue_type=corporate&page=4")
 
 wait = WebDriverWait(driver, 10)
-venue_names = []
 
-# initial list of panels
-panels = wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, "panel")))
+# click only the first panel
+first_panel = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "panel")))
+first_panel.click()
 
-for i in range(len(panels)):
-    panels = wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, "panel")))
-    panels[i].click()
+# scrape the venue name
+venue_name_element = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "article-heading")))
+venue_name = venue_name_element.text
 
-    # wait for the venue name to load on the new page
-    venue_name_element = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "article-heading")))
-    venue_names.append(venue_name_element.text)
+# insert venue name into Airtable
+data = {
+    "fields": {
+        "Name": venue_name  # Ensure this matches the exact field label in Airtable
+    }
+}
+response = requests.post(airtable_url, headers=headers, json=data)
+if response.status_code == 200:
+    print(f"Successfully added {venue_name} to Airtable.")
+else:
+    print(f"Failed to add {venue_name} to Airtable. Status Code: {response.status_code}, Response: {response.text}")
 
-    # navigate back to main page
-    driver.back()
-
-# Output 
-for name in venue_names:
-    print(f"Venue Name: {name}")
-
+# Navigate back if necessary, or close the driver if done
 driver.quit()
+
+# Output the venue name
+print(f"Venue Name: {venue_name}")
